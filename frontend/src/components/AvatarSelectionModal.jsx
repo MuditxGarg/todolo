3cae3f4e2bf74966eb4723e0cb04b27743c46c39
OjaswiniKohale <ojaswini751@gmail.com>
2023-10-03 10:47:15 +0530

    added comments to the component

--- frontend/src/components/AvatarSelectionModal.jsx
+++ frontend/src/components/AvatarSelectionModal.jsx
@@ -2,27 +2,36 @@ import React, { useState, useEffect } from 'react';
 import Modal from '@mui/material/Modal';
 import { Box, Button } from '@mui/material';
 
+// Define a functional component named 'AvatarSelectionModal' that accepts 'isOpen', 'onClose', and 'onSelect' props.
 function AvatarSelectionModal({ isOpen, onClose, onSelect }) {
+  // Define a state variable 'avatarImages' to store the imported avatar image paths.
   const [avatarImages, setAvatarImages] = useState([]);
 
+  // Use the 'useEffect' hook to run the image import logic when the component mounts.
   useEffect(() => {
+    // Define an asynchronous function to import avatar images.
     const importImages = async () => {
       const importPromises = [];
+      // Loop through avatar image numbers from 1 to 24 and import each image.
       for (let i = 1; i <= 24; i++) {
         importPromises.push(import(`../assets/avatars/avatar_${i}.png`));
       }
 
       try {
+        // Wait for all import promises to resolve and store the image paths.
         const avatarImagePaths = await Promise.all(importPromises);
+        // Extract the 'default' property from each imported module and set it in the state.
         setAvatarImages(avatarImagePaths.map((module) => module.default));
       } catch (error) {
         console.error('Error importing avatar images:', error);
       }
     };
 
+    // Call the 'importImages' function when the component mounts (empty dependency array).
     importImages();
   }, []);
 
+  // Render a modal dialog with avatar selection options.
   return (
     <Modal
       open={isOpen}
@@ -32,6 +41,7 @@ function AvatarSelectionModal({ isOpen, onClose, onSelect }) {
     >
       <Box
         sx={{
+          // Style for the modal content.
           position: 'absolute',
           top: '50%',
           left: '50%',
@@ -51,6 +61,7 @@ function AvatarSelectionModal({ isOpen, onClose, onSelect }) {
         <h2 style={{ color: '#155360' }}>Select Avatar</h2>
         <div>
           {avatarImages.map((avatar, index) => (
+            // Render each avatar image as an <img> element with an 'onClick' event.
             <img
               key={index}
               src={avatar}
@@ -63,9 +74,14 @@ function AvatarSelectionModal({ isOpen, onClose, onSelect }) {
         <Button
           onClick={onClose}
           variant="contained"
-          sx={{ backgroundColor: '#155360', color: '#FFFFFF', marginTop: '10px', '&:hover': {
-            backgroundColor: 'rgba(52, 196, 181, 1)',
-          },}}
+          sx={{
+            backgroundColor: '#155360',
+            color: '#FFFFFF',
+            marginTop: '10px',
+            '&:hover': {
+              backgroundColor: 'rgba(52, 196, 181, 1)',
+            },
+          }}
         >
           Cancel
         </Button>
@@ -74,4 +90,5 @@ function AvatarSelectionModal({ isOpen, onClose, onSelect }) {
   );
 }
 
+// Export the 'AvatarSelectionModal' component as the default export.
 export default AvatarSelectionModal;
